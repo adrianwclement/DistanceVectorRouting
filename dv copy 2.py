60365f2553dd88f2ff8d6a84c984cc7033116fe9
Ben Cerbin <bencerbin@MacBook-Air-von-Ben.local>
2025-11-18 21:45:18 -0800

    disable and crash

--- dv copy 2.py
+++ dv copy 2.py
@@ -10,7 +10,6 @@ import socket
 import threading
 import time
 import sys
-import os
 from math import inf
 from collections import defaultdict
 
@@ -188,10 +187,8 @@ class DVServer:
                 continue
 
     def handle_incoming(self, msg, addr):
-        # NEW: process link_update or crash control message BEFORE normal DV handling
-        msg_type = msg.get("type")
-        
-        if msg_type == "link_update":
+        # NEW: process link_update control message BEFORE normal DV handling
+        if msg.get("type") == "link_update":
             try:
                 a = int(msg["from"])
             except Exception:
@@ -207,23 +204,7 @@ class DVServer:
                 self.routing_table[a] = {"cost": cost, "next_hop": a if cost < INF else -1}
             return
 
-        elif msg_type == "crash":
-            # neighbor has crashed — mark all links INF and crash this server too
-            with LOCK:
-                for n in list(self.neighbors):
-                    self.neighbor_costs[n] = INF
-                    self.disable_set.add(n)
-                    if n in self.routing_table:
-                        self.routing_table[n]['cost'] = INF
-                        self.routing_table[n]['next_hop'] = -1
-                self.crashed = True
-            print(f"CRASH received from server {msg.get('from')}, shutting down...")
-            try:
-                self.sock.close()
-            finally:
-                os._exit(0)
-
-        # Expecting JSON as per our format (normal DV packet)
+        # Expecting JSON as per our format
         try:
             sender_id = int(msg.get('sender_id'))
             sender_ip = msg.get('sender_ip')
@@ -275,9 +256,14 @@ class DVServer:
                     self.routing_table[dest]['cost'] = new_cost
                     self.routing_table[dest]['next_hop'] = sender_id
                     changed = True
-        # End handle_incoming
-        return
-
+            # if neighbor gone / reported INF to something, we won't remove entries automatically
+            # but timeouts handle neighbors disappearance
+            # (assignment requires not removing server from table, just set cost to infinity)
+            # nothing further to do
+            # We do not trigger immediate updates on change (assignment specification).
+            # So only print success message of receiving is required (done above).
+            # End handle_incoming
+            return
 
     def build_dv_packet(self):
         # Build the dv as list of dicts including this server's view of each dest
@@ -454,25 +440,31 @@ class DVServer:
                             if x in self.routing_table:
                                 self.routing_table[x]['cost'] = INF
                                 self.routing_table[x]['next_hop'] = -1
+
+                            ### NEW: send link_update on disable (bi-directionality)
+                            info = self.servers.get(x)
+                            if info:
+                                control = {
+                                    "type": "link_update",
+                                    "from": self.my_id,
+                                    "to": x,
+                                    "cost": "inf"
+                                }
+                                try:
+                                    self.sock.sendto(json.dumps(control).encode(),
+                                                    (info["ip"], info["port"]))
+                                except:
+                                    pass
+
                             print(f"{cmd} SUCCESS")
                         else:
                             print(f"{cmd} ERROR: {x} is not a neighbor")
 
                 elif parts[0].lower() == 'crash':
-                    # propagate crash to all neighbors first
+                    # close all connections. Set all neighbor costs to INF and exit
                     with LOCK:
-                        crash_msg = {"type": "crash", "from": self.my_id}
-                        for n in list(self.neighbors):
-                            info = self.servers.get(n)
-                            if info:
-                                try:
-                                    self.sock.sendto(json.dumps(crash_msg).encode(), (info["ip"], info["port"]))
-                                except:
-                                    pass
-                        # mark all links as down locally
                         for n in list(self.neighbors):
                             self.neighbor_costs[n] = INF
-                            self.disable_set.add(n)
                             if n in self.routing_table:
                                 self.routing_table[n]['cost'] = INF
                                 self.routing_table[n]['next_hop'] = -1
